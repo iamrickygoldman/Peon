1f8a27644f31a42d853c474933a4727690c9aca3
Ricky Goldman <luigijerk@yahoo.com>
2016-11-15 01:14:16 -0600

    working with cmd args

--- Peon/PeonHelper.fs
+++ Peon/PeonHelper.fs
@@ -1,13 +1,47 @@
 ﻿module PeonHelper
 
-let getDirectories (path:string) :System.IO.DirectoryInfo[] =
-    let dir = new System.IO.DirectoryInfo(path)
-    dir.GetDirectories()
-
 let cleanFileName (name:string) :string =
     let stripChars text (chars:string) =
         Array.fold (fun (s:string) c-> s.Replace(c.ToString(), "")) text (chars.ToCharArray())
     stripChars name "<>:\"/\\|?*"
+
+let formatName (format:string) (file:TagLib.File) :string =
+    let tag = file.Tag
+    let mutable n:string = tag.Track.ToString()
+    let mutable N:string = n
+    if (tag.Track = 0u) then
+        n <- "0"
+        N <- "00"
+    elif (tag.Track < 10u) then
+        N <- "0" + tag.Track.ToString()
+    let mutable T:string = ""
+    if not (tag.Title = null) then
+        T <- tag.Title
+    let t:string = T.ToLower()
+    let mutable A:string = "";
+    if not (tag.FirstAlbumArtist = null) then
+        A <- tag.FirstAlbumArtist
+    elif not (tag.FirstPerformer = null) then
+        A <- tag.FirstPerformer
+    elif not (tag.FirstComposer = null) then
+        A <- tag.FirstComposer
+    let a:string = A.ToLower()
+    let mutable B:string = ""
+    if not (tag.Album = null) then
+        B <- tag.Album
+    let b:string = B.ToLower()
+
+    let mutable formatted = format
+    formatted <- formatted.Replace("?N",N)
+    formatted <- formatted.Replace("?n",n)
+    formatted <- formatted.Replace("?T",T)
+    formatted <- formatted.Replace("?t",t)
+    formatted <- formatted.Replace("?A",A)
+    formatted <- formatted.Replace("?a",a)
+    formatted <- formatted.Replace("?B",B)
+    formatted <- formatted.Replace("?b",b)
+    printfn "%s" formatted
+    (cleanFileName formatted)
 
 let getAudioMimeTypes :string[] =
     [|
--- Peon/PeonMain.fs
+++ Peon/PeonMain.fs
@@ -1,29 +1,62 @@
 ﻿[<EntryPoint>]
 let main argv = 
-    let path = "e:\\NET\TEST"
-    let dir = new System.IO.DirectoryInfo(path)
-    let extensions = PeonHelper.getAudioMimeTypes
-    for artist in dir.GetDirectories() do
-        printfn "Artist: %s" artist.Name
-        for album in artist.GetDirectories() do
-            printfn "\t Album: %s" album.Name
-            for song in album.GetFiles() do
-                if (Array.contains song.Extension extensions) then
-                    let mutable songTags = null;
-                    try
-                        songTags <- TagLib.File.Create(song.FullName).Tag
-                    with
-                        | :? TagLib.UnsupportedFormatException -> printfn "File format not allowed!"
-                    
-                    let mutable trackString = songTags.Track.ToString() + " - ";
+    if argv.Length = 0 then
+        printfn "You must specify either a full directory path or use %sCD%s for current directory" "%" "%"
+    elif argv.[0] = "--help" then
+        printfn "Peon.exe music_dir [format] [space_replace]";
+        printfn "\n\r music_dir -> absolute path to top level music directory"
+        printfn "    format -> music file naming format e.g. ?N - ?T"
+        printfn "underscore -> replace spaces with entered value"
+        printfn "\n\rFor full list of format values, use --help-format."
+        printfn "\n\rDirectories must be structured as:"
+        printfn "Top"
+        printfn "|-Artist_Name"
+        printfn "|-|-Album_Name"
+    elif (argv.[0] = "--help-format") then
+        printfn "Format Values:"
+        printfn "?N -> track number with leading 0 (ex. 09)"
+        printfn "?n -> track number (ex: 9)"
+        printfn "?T -> title"
+        printfn "?t -> title in lowercase"
+        printfn "?A -> artist"
+        printfn "?a -> artist in lowercase"
+        printfn "?B -> album"
+        printfn "?b -> album in lowercase"
+    else
+        let path = argv.[0]
+        let mutable format = "?N - ?T"
+        if argv.Length > 1 then
+            format <- argv.[1]
+        if not (format.Contains("?")) then
+            printfn "Your format string doesn't contain any wildcards and will not generate unique filenames."
+        else
+            try
+                let dir = new System.IO.DirectoryInfo(path)
+                let extensions = PeonHelper.getAudioMimeTypes
+                for artist in dir.GetDirectories() do
+                    printfn "Artist: %s" artist.Name
+                    for album in artist.GetDirectories() do
+                        printfn "\t Album: %s" album.Name
+                        for song in album.GetFiles() do
+                            if (Array.contains song.Extension extensions) then
+                                let mutable songTags = null
+                                try
+                                    songTags <- TagLib.File.Create(song.FullName)
+                                with
+                                    | :? TagLib.UnsupportedFormatException -> printfn "File format not allowed!"
+                                let mutable newName:string = PeonHelper.formatName format songTags
+                                if argv.Length > 2 then
+                                    newName <- newName.Replace(" ",argv.[2])
+                                song.MoveTo(song.DirectoryName + "\\" + newName + song.Extension)
+                                printfn "\t\t Song: %s" (song.FullName)
+            with
+                | :? System.ArgumentNullException -> printfn "Path is null."
+                | :? System.Security.SecurityException -> printfn "You lack permission to edit this directory."
+                | :? System.ArgumentException -> printfn "Path contains invalid characters."
+                | :? System.IO.PathTooLongException -> printfn "Path name is too long."
+                | :? System.IO.DirectoryNotFoundException -> printfn "Directory not found."
+                | :? System.IO.IOException -> printfn "Path is a file. Choose a directory."
 
-                    if (songTags.Track = 0u) then
-                        trackString <- "";
-                    elif (songTags.Track < 10u) then
-                        trackString <- "0" + songTags.Track.ToString() + " - "
-                    let newName:string = song.DirectoryName + "\\" + trackString + PeonHelper.cleanFileName songTags.Title + song.Extension
-                    song.MoveTo(newName)
-                    printfn "\t\t Song: %s" (song.FullName)
-
+    printfn "\n\rPress any key to exit"
     System.Console.ReadKey() |> ignore
     0
